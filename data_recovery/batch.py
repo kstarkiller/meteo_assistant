e6340398a8d3953ecef0ed6caba9a8e783904ba1
Kevin SIMON <simonkvin@gmail.com>
2024-03-21 13:17:12 +0100

    Mise à jour du schéma de la base de données et des tables, ajout de la colonne "date" de type DateTime, suppression des données précédentes

--- data_recovery/batch.py
+++ data_recovery/batch.py
@@ -3,37 +3,38 @@ from meteofrance_api import MeteoFranceClient
 from cities import cities
 from functions.db_and_table_init import connect_to_database, create_table
 from functions.insert_data import insert_weather_data
+from functions.delete_data import delete_former_data
 
-def fetch_weather_data():
-    # Init client
-    client = MeteoFranceClient()
+# Init client
+client = MeteoFranceClient()
 
-    # Connect to the database
-    conn = connect_to_database()
+# Connect to the database
+conn = connect_to_database()
 
-    # Create the table if it doesn't exist
-    create_table(conn)
+# Create the table if it doesn't exist
+create_table(conn)
 
-    # Iterate over cities
-    for c in cities:
-        # Search a location from name
-        list_places = client.search_places(c)
-        my_place = list_places[0]
+# Iterate over cities and fetch weather data
+for c in cities:
+    # Search a location from name
+    list_places = client.search_places(c)
+    my_place = list_places[0]
 
-        # Fetch weather forecast for the location
-        my_place_weather_forecast = client.get_forecast_for_place(my_place)
+    # Fetch weather forecast for the location
+    my_place_weather_forecast = client.get_forecast_for_place(my_place)
 
-        # Get the daily forecast
-        my_place_daily_forecast = my_place_weather_forecast.daily_forecast
+    # Get the daily forecast
+    my_place_daily_forecast = my_place_weather_forecast.daily_forecast
 
-        # Insert the data into the database
-        for day in my_place_daily_forecast:
-            insert_weather_data(conn, client, my_place, my_place_weather_forecast, day)
+    # Insert the data into the database
+    for day in my_place_daily_forecast:
+        insert_weather_data(conn, client, my_place, my_place_weather_forecast, day)
 
-    # Commit the transaction
-    conn.commit()
+# Delete former data
+delete_former_data(conn)
 
-    # Close the connection
-    conn.close()
+# Commit the transaction
+conn.commit()
 
-fetch_weather_data()
+# Close the connection
+conn.close()
