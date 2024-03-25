96168f8fb6fde5c4399fa969542ba8e078de64ca
Kevin SIMON <simonkvin@gmail.com>
2024-03-25 13:25:20 +0100

    Mise à jour du générateur de rapports météo

--- weather_repor_gen/nlp.py
+++ weather_repor_gen/nlp.py
@@ -5,7 +5,9 @@ from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 import json
 import requests
-from datetime import datetime, time
+from datetime import time
+from typing import Optional
+import base64
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '../../'))
 from hidden import B16_API_KEY
@@ -14,8 +16,32 @@ from retrieve_data_from_db import fetch_data_from_db
 
 # Set the API key and the headers
 headers = {"Authorization": "Bearer " + B16_API_KEY}
-provider = "openai"
-url = "https://api.edenai.run/v2/text/generation"
+text_provider = "mistral"
+speech_provider = "google"
+text_url = "https://api.edenai.run/v2/text/generation"
+speech_url = "https://api.edenai.run/v2/audio/text_to_speech"
+
+text_payload = {
+            "response_as_dict": True,
+            "attributes_as_list": False,
+            "show_original_response": False,
+            "providers": text_provider,
+            "temperature": 0.2,
+            "max_tokens": 250,
+        }
+
+speech_payload = {
+        "response_as_dict": True,
+        "attributes_as_list": False,
+        "show_original_response": False,
+        "rate": 0,
+        "pitch": 0,
+        "volume": 0,
+        "sampling_rate": 0,
+        "providers": speech_provider,
+        "language": "en-GB",
+        "option": "FEMALE" 
+    }
 
 app = FastAPI()
 
@@ -33,26 +59,47 @@ app.add_middleware(
 async def read_item(prompt):
     return {"It works"}
 
-@app.post("/{city}/{date}/{hour}")
-async def bot_request(city: str, date: str, hour: int):
-    # Convert hour to time object
-    hour = time(hour,0,0)
+@app.post("/weather_request/")
+async def bot_request(city: str, date: str, hour: Optional[int] = None):
+    if hour is not None:
+        # Convert hour to time object
+        hour = time(hour, 0, 0)
 
     # Fetch the weather data from the database
-    weather_data = fetch_data_from_db(city, date, hour)
+    if hour:
+        weather_data = fetch_data_from_db(city, date, hour)
+        text_payload["text"] = f"Donne-moi la météo à {hour} pour {city} : {weather_data}."
 
-    payload = {
-        "providers": "openai,cohere",
-        "text": f"Donne-moi la météo d'après ces données brute : {weather_data}.",
-        "temperature": 0.2,
-        "max_tokens": 250,
-        "fallback_providers": ""
-    }
+    else:
+        weather_data = fetch_data_from_db(city, date)
+        text_payload["text"] = f"Donne-moi la météo moyenne du jour pour {city} : {weather_data}."
 
-    response = requests.post(url, json=payload, headers=headers)
-    result = json.loads(response.text)[provider]
+    text_response = requests.post(text_url, json=text_payload, headers=headers)
+    text_result = json.loads(text_response.text)[text_provider]['generated_text']
 
-    return result['generated_text']
+    # Get the speech response
+    speech_payload["text"] = text_result
+    speech_response = requests.post(speech_url, json=speech_payload, headers=headers)
+    speech_result = json.loads(speech_response.text)[speech_provider]['audio']
+    
+    if text_response.status_code == 200:
+        if speech_response.status_code == 200:
+            if speech_result:
+                audio_bytes = base64.b64decode(speech_result)
+                with open("audio_result/audio.mp3", "wb") as audio_file:
+                    audio_file.write(audio_bytes)
+                print(text_result)
+                return audio_bytes
+            
+            else:
+                return "Aucune donnée audio disponible dans la réponse."
+            
+        else:
+            return f"Erreur lors de la requête audio : {speech_response.status_code} - {speech_response.text}"
+        
+    else:
+        return f"Erreur lors de la requête texte : {text_response.status_code} - {text_response.text}"
+
 
 # Run the API with uvicorn on port 8000
 uvicorn.run(app, host="0.0.0.0", port=8000)
--- weather_repor_gen/retrieve_data_from_db.py
+++ weather_repor_gen/retrieve_data_from_db.py
@@ -2,7 +2,8 @@ import sys
 import os
 from sqlalchemy import create_engine, text
 from sqlalchemy.orm import sessionmaker
-from datetime import time
+# from pprint import pprint
+# from datetime import time
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir, os.path.pardir)))
 from hidden import DATABASE, USER, PASSWORD, HOST, PORT
@@ -17,29 +18,51 @@ def connect_to_database():
 
     return session
 
-def fetch_data_from_db(ville, date, heure):
+def fetch_data_from_db(ville, date, heure=None):
     # Connexion à la base de données
     conn = connect_to_database()
 
     # Requête SQL Alchemy pour récupérer les données de prévision météo
-    query = text("""
-        SELECT * FROM french_cities_weather 
-        WHERE city = :city 
-        AND date(date) = :date 
-        AND EXTRACT(HOUR FROM date) = :hour
-        AND EXTRACT(MINUTE FROM date) = :minute
-    """)
+    if heure:
+        query = text("""
+            SELECT * FROM french_cities_weather 
+            WHERE city = :city 
+            AND date(date) = :date 
+            AND EXTRACT(HOUR FROM date) = :hour
+        """)
 
-    result = conn.execute(query, {"city": ville,
+        result = conn.execute(query, {"city": ville,
                                   "date": date,
-                                  "hour": heure.hour,
-                                  "minute": heure.minute})
+                                  "hour": heure.hour})
 
-    # Construction d'un dictionnaire à partir de result
-    dict_result = {}
-    for row in result:
-        for i, column in enumerate(result.keys()):
-            if column not in ['id', 'weather_icon','add_date']:
-                dict_result[column] = row[i]
+        # Construction d'un dictionnaire à partir de result
+        hour_result = {}
+        for row in result:
+            for i, column in enumerate(result.keys()):
+                if column not in ['id', 'weather_icon','add_date']:
+                    hour_result[column] = row[i]
         
-    return dict_result
+        return hour_result
+    
+    else:
+        query = text("""
+            SELECT * FROM french_cities_weather 
+            WHERE city = :city 
+            AND date(date) = :date
+        """)
+
+        result = conn.execute(query, {"city": ville,
+                                  "date": date})
+
+        # Construction d'un dictionnaire à partir de result
+        day_result = []
+        hour_result = {}
+        for row in result:
+            for i, column in enumerate(result.keys()):
+                if column not in ['id', 'latitude', 'longitude',
+                                  'readable_warnings','weather_icon','add_date']:
+                    hour_result[column] = row[i]
+
+            day_result.append(hour_result)
+        
+        return day_result
